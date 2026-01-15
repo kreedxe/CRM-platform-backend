35eb25de8168339421805e219034ad431d25558a
kreedxe <kreedxe@gmail.com>
2026-01-15 10:33:29 +0500

    implement database connection with prisma

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
-import express, {Request, Response, NextFunction} from 'express';
 
-const app = express();
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
 
-const port = 3000;
 
-// test route
-app.get("/", (req: Request, res: Response) => {
-    res.send("Server is running...");
-});
+// load environment variables
+dotenv.config();
 
+
+const app: Application = express();
+const port = process.env.PORT || 9000;
+
+
+//setup public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// parse application/json
+app.use(express.json({ limit: "10mb" }));
+
+
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+//setup cors
+app.use(cors());
+
+
+// start server
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
-
